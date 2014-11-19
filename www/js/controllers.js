dd5d657df94af146123d1cceec84a66dd89b58ed
Hiroshi Tashiro <hiroshitash@gmail.com>
2014-11-18 21:55:21 -0800

    Comments added in chat.html

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -33,14 +33,26 @@ angular.module('starter.controllers', [])
   };
   
   $scope.chatlist = [
-    { title: "Do you like this?", people: [{'displayName':'Nick Larson'}], id: 0, image: "img/cover.jpg" },
-    { title: "What do you think of it?", people: [{'displayName':'Nick Larson'}, {'displayName':'Sophie'}, {'displayName':'Paul'}], id: 1, image: "img/cover.jpg" },
-    { title: "Should I get it?", people: [{'displayName':'Erik Archer'}, {'displayName':'Evy'}], id: 2, image: "img/cover.jpg" },
-    { title: "Buy or not buy?", people: [{'displayName':'Glenn Liao'}], id: 3, image: "img/cover.jpg" },
-    { title: "I want this", people: [{'displayName':'Alex Linda'}, {'displayName':'Alain'}], id: 4, image: "img/cover.jpg" },
-    { title: "Like it?", people: [{'displayName':'Reese'}], id: 5, image: "img/cover.jpg" },
-    { title: "Yum?", people: [{'displayName':'Sharon'}], id: 6, image: "img/cover.jpg" }
+    { title: "Do you like this?", people: [0], id: 0, image: "img/cover.jpg",
+      comments:[{comment:"comment1", contact:0},{comment:"comment2",contact:0}]},
+    { title: "What do you think of it?", people: [0, 2, 8],
+      id: 1, image: "img/cover.jpg", comments:[{comment:"comment1", contact:8},{comment:"comment2",contact:2}] },
+    { title: "Should I get it?", people: [1, 5], id: 2, image: "img/cover.jpg",
+      comments:[{comment:"comment1", contact:1},{comment:"comment2",contact:5}]},
+    { title: "Buy or not buy?", people: [3], id: 3, image: "img/cover.jpg",
+      comments:[{comment:"comment1", contact:3},{comment:"comment2",contact:-1}]},
+    { title: "I want this", people: [4, 11], id: 4, image: "img/cover.jpg",
+      comments:[{comment:"comment1", contact:4},{comment:"comment2",contact:11}]},
+    { title: "Like it?", people: [6], id: 5, image: "img/cover.jpg",
+      comments:[{comment:"comment1", contact:-1},{comment:"comment2",contact:6}]},
+    { title: "Yum?", people: [7], id: 6, image: "img/cover.jpg",
+      comments:[{comment:"comment1", contact:-1},{comment:"comment2",contact:7}]}
   ];
+  
+  $scope.contactList = [{displayName:"Nick larson"}, {displayName:"Erik Archer"}, {displayName:"Sophie"},
+                        {displayName:"Glenn Liao"}, {displayName:"Alex Linda"}, {displayName:"Evy"},
+                        {displayName:"Reese"}, {displayName:"Sharon"}, {displayName:"Paul"},
+                        {displayName:"XX2"}, {displayName:"XX3"}, {displayName:"Alain"}];
   
   $scope.showContacts = function(contacts) {
     var contactsDisplay = "";
@@ -48,7 +60,12 @@ angular.module('starter.controllers', [])
       if (prop > 0) {
         contactsDisplay += ", ";
       }
-      contactsDisplay += contacts[prop]['displayName'];
+      var contactId = contacts[prop];
+      if ($scope.contactList[contactId] == -1) {
+        contactsDisplay += "me";
+      } else {
+        contactsDisplay += $scope.contactList[contactId]['displayName'];
+      }
     }
     console.log("contactsDisplay=" + contactsDisplay);
     return contactsDisplay;
@@ -117,8 +134,19 @@ angular.module('starter.controllers', [])
 .controller('ChatlistCtrl', function($scope) {
 })
 
-.controller('ChatCtrl', ['$scope', '$stateParams',
-  function($scope, $stateParams) {
-    $scope.chat = $scope.chatlist[$stateParams.chatId];  
+.controller('ChatCtrl', ['$scope', '$stateParams', '$ionicPopup',
+  function($scope, $stateParams, $ionicPopup) {
+    $scope.chat = $scope.chatlist[$stateParams.chatId];
+    
+    $scope.sendComment = function(comment) {
+      console.log("comment sent: " + comment);
+      $scope.chat.comments.push({comment:comment,contact:-1});
+      /*
+      $ionicPopup.alert({
+              title: 'Success',
+              content: comment
+      });
+      */
+    };
   }
 ]);
